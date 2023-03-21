d6e7b66ce1bc7432f90496e034951cfee184d74f
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-21 18:56:43 -0400

    Finished schema file, three tables created in database

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,35 @@
+-- Deletes any existing database named employees_DB
 DROP DATABASE IF EXISTS employees_DB;
+-- Creates database named employees_DB
 CREATE DATABASE employees_DB;
+
+-- Uses database named employees_DB
 USE employees_DB;
 
+-- Creates table named department
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
+-- Creates table named role
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary INT NOT NULL,
-  
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+
+);
+
+-- Creates table named employee
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
