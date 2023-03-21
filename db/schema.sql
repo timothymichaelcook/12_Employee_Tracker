60c7655202735fee4307da5129f56242b2c1b181
timothymichaelcook <tmc295@email.vccs.edu>
2023-03-21 18:45:00 -0400

    Created department table with id and name

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,17 @@
 DROP DATABASE IF EXISTS employees_DB;
-
 CREATE DATABASE employees_DB;
-
 USE employees_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary INT NOT NULL,
+  
+);
 
